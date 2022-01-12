fb841b66e2778149bd778531784765045e61527e
Will McGinty <will.mcginty@bottlerocketstudios.com>
2022-01-12 14:09:51 -0600

    Small tweaks, clean up

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "DebugKit",
     platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DebugKit", targets: ["DebugKit"]),
     ],
